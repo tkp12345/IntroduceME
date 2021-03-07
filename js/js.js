da284a7ec2195495afc11d135e209ce37a2563dd
roberto <rjsdnrsla@naver.com>
2021-03-07 15:15:48 +0900

    스크롤에따른  헤더반응 자바스크립트 만들기

---
+++ js/js.js
@@ -0,0 +1,39 @@
+/*스크롤 감지 헤더 표현*/
+let header = document.querySelector('header')
+let currentScroll; 
+let lastScroll =0;
+let scroll; 
+// let currentScroll;
+
+window.addEventListener('scroll',()=>{
+    scroll = true;
+    // console.log("스크롤");
+})
+
+setInterval(()=>{
+    if(scroll){
+        console.log(currentScroll)
+        playScrolled();
+        scroll=false;
+    }
+}, 150);
+
+
+function playScrolled(){
+    currentScroll = document.documentElement.scrollTop
+  if(currentScroll <= header.offsetHeight ){
+      return;
+  }
+
+  if(lastScroll< currentScroll  ){
+  /*Scroll down */
+    header.classList.add('remove');
+  }else{
+      if(lastScroll > currentScroll )
+     /*Scroll up */
+      header.classList.remove('remove');
+    }
+
+  lastScroll = currentScroll; 
+}
+    
