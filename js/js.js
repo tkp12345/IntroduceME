04238e6e69484a851bcd394b7c2cf6db64a962bc
roberto <rjsdnrsla@naver.com>
2021-03-13 17:39:14 +0900

    메뉴페이지 마스페이지 완성

--- js/js.js
+++ js/js.js
@@ -1,5 +1,6 @@
 /*스크롤 감지 헤더 표현*/
 let header = document.querySelector('header')
+
 let currentScroll; 
 let lastScroll =0;
 let scroll; 
@@ -27,13 +28,14 @@ function playScrolled(){
   if(lastScroll< currentScroll ){
   /*Scroll down */
     header.classList.add('remove');
+  
+    
   }else{
       if(lastScroll > currentScroll )
      /*Scroll up */
       header.classList.remove('remove');
       header.classList.add('sticky');
     }
-
   lastScroll = currentScroll; 
 }
     
---
+++ js/menu.js
@@ -0,0 +1,6 @@
+function Menu(){
+    const menu = document.querySelector(".menuToggle");
+    const nav = document.querySelector(".nav");
+    menu.classList.toggle("active")
+    nav.classList.toggle("active")
+}
